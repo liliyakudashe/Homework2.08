8a1ad9f7f81f8edfafccf8d03c18e97ca918e97f
Acer <oduvan4ik_888@mail.ru>
2022-11-16 15:54:23 +0300

    На проверку

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
+import java.util.HashSet;
+import java.util.Set;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Product banana = new Product(98.56,1.1,"бананы", true);
+        Product apricot = new Product(200.52,1.4,"абрикосы",true);
+        Product grape = new Product(358.12,1.5,"виноград", true);
+        Product macadamia = new Product(1250.45,1.9,"орех макадами", true);
+        Product melon = new Product(658.47,2.5,"дыня",true);
+        Product raspberry = new Product(487.25,1.8,"малина", true);
+
+
+
+        Set<Product> list = new HashSet<>();
+        list.add(banana);
+        list.add(apricot);
+        list.add(raspberry);
+        System.out.println(list);
     }
 }
