a6782bcd5675782b9002d0d9059e0935b9af1ecb
Acer <oduvan4ik_888@mail.ru>
2022-11-18 18:59:53 +0300

    Исправила

--- src/Recipes.java
+++ src/Recipes.java
@@ -5,12 +5,10 @@ import java.util.Set;
 public class Recipes<P extends Product> {
     private String names;
     private List<Product> products;
-    private double theTotalCost;
 
     public Recipes(String name, List<Product> products) {
         this.names = name;
         this.products = products;
-        this.theTotalCost = theTotalCost;
     }
 
     public String getName() {
@@ -22,6 +20,7 @@ public class Recipes<P extends Product> {
     }
 
     public double getTheTotalCost() {
+        double theTotalCost = 0;
         for (int i = 0; i < (products.size()); i++){
             theTotalCost += products.get(i).getPrice();
         }
@@ -40,11 +39,11 @@ public class Recipes<P extends Product> {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Recipes<?> recipes = (Recipes<?>) o;
-        return Double.compare(recipes.theTotalCost, theTotalCost) == 0 && Objects.equals(names, recipes.names) && Objects.equals(products, recipes.products);
+        return Objects.equals(names, recipes.names) && Objects.equals(products, recipes.products);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(names, products, theTotalCost);
+        return Objects.hash(names, products);
     }
 }
